f73807dd817a321f8a3fecc979ea3c941d900788
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro-2.local>
2022-04-07 00:20:36 +0530

    Added nDCG

--- Q2.py
+++ Q2.py
@@ -26,6 +26,12 @@ def get_DCG(relevances):
         dcg += (2 ** rel - 1) / math.log2(i + 2)
     return dcg
 
+def get_nDCG(relevances):
+    ndcg = 0
+    for i, rel in enumerate(relevances):
+        ndcg += (2 ** rel - 1) / math.log2(i + 2)
+    return ndcg/get_DCG(sorted(relevances, reverse=True))
+
 path = "Data/IR-assignment-2-data.pkl"
 data = joblib.load(path)
 
@@ -53,3 +59,7 @@ for key, val in rel_counter.items():
     number_of_files_with_max_dcg *= math.factorial(val)
 
 print("Number of files with max DCG:", number_of_files_with_max_dcg)
+print()
+
+print("nDCG of original data at 50:", get_nDCG(relevances[:50]))
+print("nDCG of whole dataset:", get_nDCG(relevances))
