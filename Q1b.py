c50fe2b9eed6760b9401cb3bd17dc6a0b9d51852
Shaunak Pal <palshaunak7@gmail.com>
2022-04-07 12:40:01 +0530

    Update

--- Q1b.py
+++ Q1b.py
@@ -1,3 +1,4 @@
+from multiprocessing import Pool, current_process, freeze_support
 import joblib
 from collections import Counter
 import numpy as np
@@ -20,17 +21,18 @@ def getTermFrequency(term: str, doc: dict, postingsList: dict[dict[str]], method
         return 0.5 + 0.5 * (postingsList[term][doc['file']] / Counter(doc['filtered_content']).most_common(1)[0][1])
 
 
-def getInverseDocumentFrequency(term: str, data: list[dict], docName: str, postingsList: dict[dict[str]]) -> float:
+def getInverseDocumentFrequency(term: str, data: list[dict], postingsList: dict[dict[str]]) -> float:
     """
     This function calculates the inverse document frequency of a word in a document.
     IDF(word) = log(total no. of documents / document frequency(word)+1)
     """
-    return np.log(1 + len(data) / postingsList[term][docName])
+    return np.log(1 + len(data) / (len(postingsList[term]) + 1))
 
 
 def getPostingsList(data: list[dict]):
     print("Getting Postings List")
     postingsList = {}
+    # tqdm inside starmap_async
     for doc in tqdm(data):
         for term in doc['filtered_content']:
             if term not in postingsList:
@@ -40,15 +42,23 @@ def getPostingsList(data: list[dict]):
     return postingsList
 
 
-def getTFIDFVector(data: list[dict], postingsList: dict[dict[str]], tfMethod: str) -> np.array:
+def getTFIDFVector(data: list[dict], postingsList: dict[dict[str]], tfMethod: str, save: bool = False) -> np.array:
     """
     This function calculates the TF-IDF Vector of all documents.
     """
     tfidfVector = np.zeros((len(data), len(postingsList)))
-    for i, doc in enumerate(tqdm(data)):
-        for j, term in enumerate(postingsList):
-            if term in doc['filtered_content']:
-                tfidfVector[i, j] = getTermFrequency(term, doc, postingsList, tfMethod) * getInverseDocumentFrequency(term, data, doc['file'], postingsList)
+    current = current_process()
+    pos = current._identity[0] - 1
+
+    # Calculate TF-IDF Vector for each document
+    with tqdm(total=len(data), desc=tfMethod, position=pos) as pbar:
+        for i, doc in enumerate(data):
+            for j, term in enumerate(postingsList):
+                if term in doc['filtered_content']:
+                    tfidfVector[i, j] = getTermFrequency(term, doc, postingsList, tfMethod) * getInverseDocumentFrequency(term, data, postingsList)
+            pbar.update(1)
+
+    joblib.dump(tfidfVector, f'Data/tfidfVector_{tfMethod}.pkl', compress=9)
     return tfidfVector
 
 
@@ -60,11 +70,12 @@ def runner():
     # joblib.dump(postingsList, 'Data/postingsList.pkl', compress=9)
     postingsList = joblib.load('Data/postingsList.pkl')
 
-    for method in ['binary', 'raw_count', 'term_frequency', 'log_normalization', 'double_normalization']:
-        print(method)
-        tfidfVector = getTFIDFVector(data, postingsList, method)
-        joblib.dump(tfidfVector, f'Data/tfidfVector_{method}.pkl', compress=9)
+    # Paralellize
+    methods = ['binary', 'raw_count', 'term_frequency', 'log_normalization', 'double_normalization']
+    with Pool(processes=4) as pool:
+        pool.starmap_async(getTFIDFVector, [(data, postingsList, method) for method in methods]).get()
 
 
 if __name__ == "__main__":
+    freeze_support()
     runner()
